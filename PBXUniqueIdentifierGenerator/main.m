ece593d4658ee64f294a8d69ec8a0ed1322b4ef2
BoHong Li <a60814billy@gmail.com>
2026-01-30 06:30:46 +0800

    feat(main): implement loop for generating multiple unique identifiers

--- PBXUniqueIdentifierGenerator/main.m
+++ PBXUniqueIdentifierGenerator/main.m
@@ -6,13 +6,20 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSUserDefaults *args = [NSUserDefaults standardUserDefaults];
         NSString *baseId = [args stringForKey:@"baseid"];
+        NSUInteger count = [args integerForKey:@"count"];
+        if (count == 0) {
+            count = 1;
+        }
         
         if (baseId) {
             [PBXUniqueIdentifierGenerator restoreGlobalStateFromIdentifier:baseId];
         }
 
-        NSString *newObjId = [NSString stringWithHexadecimalRepresentationOfUniqueIdentifier];
-        printf("%s\n", [newObjId cStringUsingEncoding:NSUTF8StringEncoding]);
+        for (NSUInteger i = 0; i < count; i++)
+        {
+            NSString *newObjId = [NSString stringWithHexadecimalRepresentationOfUniqueIdentifier];
+            printf("%s\n", [newObjId cStringUsingEncoding:NSUTF8StringEncoding]);
+        }
     }
     return EXIT_SUCCESS;
 }
